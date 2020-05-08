0a62aee073012e7f49cb880c879854bc1353f91d
jhuniorQUIGA <48796947+jhuniorQUIGA@users.noreply.github.com>
2020-05-08 14:53:19 -0400

    Actualizacion de archivos de configuracion

---
+++ app/src/main/java/com/icar/demy/schoolmovil/actividades/registroProfesorActivity.java
@@ -0,0 +1,16 @@
+package com.icar.demy.schoolmovil.actividades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.icar.demy.schoolmovil.R;
+
+public class registroProfesorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_profesor);
+    }
+}
