cfc0ed470bd1cc0252f84639adee56acb561a82d
jhuniorQUIGA <48796947+jhuniorQUIGA@users.noreply.github.com>
2020-05-08 14:45:54 -0400

    Actualizacion de archivos de configuracion

--- app/src/main/java/com/icar/demy/schoolmovil/actividades/SplashActivity.java
+++ app/src/main/java/com/icar/demy/schoolmovil/actividades/SplashActivity.java
@@ -11,6 +11,6 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash2);
+        setContentView(R.layout.activity_splash);
     }
 }
