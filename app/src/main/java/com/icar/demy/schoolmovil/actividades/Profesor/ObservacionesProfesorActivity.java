df6752a69141d74d81abbd7f77dd595bb511a523
jhuniorQUIGA <48796947+jhuniorQUIGA@users.noreply.github.com>
2020-07-11 19:56:55 -0400

    ultimos cambios

---
+++ app/src/main/java/com/icar/demy/schoolmovil/actividades/Profesor/ObservacionesProfesorActivity.java
@@ -0,0 +1,16 @@
+package com.icar.demy.schoolmovil.actividades.Profesor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.icar.demy.schoolmovil.R;
+
+public class ObservacionesProfesorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_observaciones_profesor);
+    }
+}
