9e57070173f456760e3f5dcfdbbf8991c7d90509
estebanluis <luisestebanescobarrocha@gmail.com>
2020-07-12 11:56:32 -0400

    interfaces

---
+++ app/src/main/java/com/icar/demy/schoolmovil/activity_entrevista_profesor.java
@@ -0,0 +1,14 @@
+package com.icar.demy.schoolmovil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_entrevista_profesor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entrevista_profesor3);
+    }
+}
---
+++ app/src/main/java/com/icar/demy/schoolmovil/activity_seleccion_materia.java
@@ -0,0 +1,13 @@
+package com.icar.demy.schoolmovil;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_seleccion_materia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seleccion_materia);
+    }
+}
