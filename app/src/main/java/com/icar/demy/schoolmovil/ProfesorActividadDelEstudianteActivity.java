1237401bb22626cb6d94380de0d00adeec182b42
Jesus <cjesusjhonatan@gmail.com>
2020-07-13 11:27:09 -0400

    nueva rama

--- app/src/main/java/com/icar/demy/schoolmovil/ProfesorActividadDelEstudianteActivity.java
+++ app/src/main/java/com/icar/demy/schoolmovil/ProfesorActividadDelEstudianteActivity.java
@@ -11,4 +11,5 @@ public class ProfesorActividadDelEstudianteActivity extends AppCompatActivity {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_profesor_actividad_del_estudiante );
     }
+    //JesusActu
 }
