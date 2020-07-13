3418a22bd925dfe40c44dbb0c4078dbb6457c77e
saisem <48866489+saisem@users.noreply.github.com>
2020-07-13 10:30:34 -0400

    union a firebase

--- app/src/main/java/com/icar/demy/schoolmovil/MainActivity.java
+++ app/src/main/java/com/icar/demy/schoolmovil/MainActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.icar.demy.schoolmovil.actividades.AdminRegisProfeActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ app/src/main/java/com/icar/demy/schoolmovil/ProfesorActividadDelEstudianteActivity.java
@@ -0,0 +1,14 @@
+package com.icar.demy.schoolmovil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfesorActividadDelEstudianteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_profesor_actividad_del_estudiante );
+    }
+}
