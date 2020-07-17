469de88769787eecea7a736ed27834f975608607
Jesus <cjesusjhonatan@gmail.com>
2020-07-17 14:54:26 -0400

    17-14:54 registro de anuncios genereales admin

---
+++ app/src/main/java/com/icar/demy/schoolmovil/Registros/Anuncios.java
@@ -0,0 +1,33 @@
+package com.icar.demy.schoolmovil.Registros;
+
+public class Anuncios {
+
+    String anunciosId;
+    String anuncios;
+    String personalAnuncios;
+    public Anuncios(String anunciosId, String anuncios, String personalAnuncios) {
+        this.anunciosId = anunciosId;
+        this.anuncios = anuncios;
+        this.personalAnuncios = personalAnuncios;
+    }
+    public String getAnunciosId() {
+        return anunciosId;
+    }
+
+    public String getAnuncios() {
+        return anuncios;
+    }
+
+    public String getPersonalAnuncios() {
+        return personalAnuncios;
+    }
+
+
+
+
+
+
+
+
+
+}
--- app/src/main/java/com/icar/demy/schoolmovil/actividades/AdminAnunciosGenerales.java
+++ app/src/main/java/com/icar/demy/schoolmovil/actividades/AdminAnunciosGenerales.java
@@ -4,24 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.icar.demy.schoolmovil.ObservacionesProfesorActivity;
 import com.icar.demy.schoolmovil.R;
+import com.icar.demy.schoolmovil.Registros.Anuncios;
+import com.icar.demy.schoolmovil.Registros.Estudiantes;
 
 
 public class AdminAnunciosGenerales extends AppCompatActivity {
     Spinner regist;
     Button irRegistros;
+
+    Button enviarAnun;
+    EditText anuncio;
+    Spinner personalAnu;
+
     Integer pos ;
+
+    private DatabaseReference Anuncios;
+// ...
+
     //String[] items;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_admin_anuncios_generales );
+
+        Anuncios = FirebaseDatabase.getInstance().getReference("Anuncios");
 
         irRegistros = (Button) findViewById(R.id.irRegistros) ;
         regist = (Spinner) findViewById(R.id.registros1);
@@ -44,5 +62,29 @@ public class AdminAnunciosGenerales extends AppCompatActivity {
                 }
             }
         });
+
+        anuncio = (EditText) findViewById(R.id.anunciosDireccion);
+        personalAnu = (Spinner) findViewById(R.id.spinPersonal);
+        enviarAnun = (Button) findViewById(R.id.btnEnviarAnuncio);
+
+        enviarAnun.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registrarAnuncios();
+            }
+        });
+    }
+
+    public void registrarAnuncios (){
+        String anu = anuncio.getText().toString();
+        String personalAnuncio = personalAnu.getSelectedItem().toString();
+        if (TextUtils.isEmpty(anu)) {
+            Toast.makeText(this, "Debe llenar los campos", Toast.LENGTH_LONG).show();
+        }else{
+            String idAnuncio = Anuncios.push().getKey();
+            Anuncios anuncio = new Anuncios(idAnuncio,anu,personalAnuncio);
+            Anuncios.child("Anuncios").child(idAnuncio).setValue(anuncio);
+            Toast.makeText(this,"Registrado",Toast.LENGTH_LONG).show();
+        }
     }
 }
